a06ec1b21c66f485bcdec8ec8f985d127d15312f
rkalusniak <rachel.kalusniak@gmail.com>
2023-05-13 16:12:32 -0400

    add comments to Day 2 code

--- Day2/ProgrammingPuzzle2a.py
+++ Day2/ProgrammingPuzzle2a.py
@@ -5,6 +5,7 @@ by: Rachel Kalusniak
 """
 import pandas as pd
 
+#Allow to run with test or actual file
 test = False
 if test:
     data_file = 'input_test.txt'
@@ -12,25 +13,33 @@ else:
     data_file = 'input.txt'
 
 
+#Open txt file and read into list
 with open(data_file, 'r') as f:
     input = f.read().splitlines()
 
+#Split list into sub list with direction and number
 input_edit = [input[x].split(" ", 1) for x in range(0, len(input))]
 
 direct = [i[0] for i in input_edit]
 number = [int(i[1]) for i in input_edit]
 
+#Turn direction and numbers list into pandas dataframe
 directions = pd.DataFrame(data = {'direct': direct, 'number': number})
 
+#Group data frame by direction
 count = directions.groupby('direct').sum().reset_index()
-
 count.set_index('direct', inplace= True)
 
+#Extract the sum of directions from the group by data frame
 down = count.loc['down']['number']
 up = count.loc['up']['number']
 forward = count.loc['forward']['number']
+
+#Find the net vertical direction and answer
 vertical = down - up
 product_directions = vertical * forward
 
+
+#Print the count dataframe and AoC answer
 print(count)
 print(product_directions)
--- Day2/ProgrammingPuzzle2b.py
+++ Day2/ProgrammingPuzzle2b.py
@@ -3,24 +3,31 @@ Created: May 13 15:28:31 2023
 
 by: Rachel Kalusniak
 """
-import pandas as pd
 
+#Allow to run with test or actual file
 test = False
 if test:
     data_file = 'input_test.txt'
 else:
     data_file = 'input.txt'
 
+
+#Open txt file and split into list
 with open(data_file, 'r') as f:
     input = f.read().splitlines()
 
+#Split into direction and number list
 input_edit = [input[x].split(" ", 1) for x in range(0, len(input))]
 direct = [i[0] for i in input_edit]
 number = [int(i[1]) for i in input_edit]
 
+
+#Initialize total direction variable
 horizontal = 0
 depth = 0
 aim = 0
+
+#Loop through number and directional lists with new rules to compute total
 for i in range(0, len(input_edit)):
     if direct[i] == 'forward':
         horizontal += number[i]
@@ -30,7 +37,10 @@ for i in range(0, len(input_edit)):
     if direct[i] == 'down':
         aim += number[i]
 
+#Compute answer
 answer = horizontal * depth
 
+
+#Print results
 print(f'The horizontal position is {horizontal}, and the depth is {depth}.')
 print(answer)
